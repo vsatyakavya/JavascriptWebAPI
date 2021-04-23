b1c96af573e2bea2def177c8f7d902fc1ab28af1
satya kavya <satyakavya24@gmail.com>
2021-04-23 10:41:44 -0500

    modified css effects using javavscript

---
+++ WEB-API/Activities/Traverse-the -DOM/script.js
@@ -0,0 +1,17 @@
+var articlesVar = document.getElementById("articles");
+
+var mainDiv = document.getElementById("main");
+
+
+articlesVar.children[0].lastElementChild.style.color="blue";
+articlesVar.children[0].style.fontSize = "50px";
+articlesVar.previousElementSibling.style.background="black";
+
+
+// mainDiv.style.marginBottom="250px";
+
+mainDiv.childNodes[1].style.textDecoration = "underline";
+mainDiv.lastElementChild.style.fontSize="25px";
+mainDiv.firstElementChild.style.color="orange";
+
+ mainDiv.lastElementChild.parentElement.style.fontSize="50px";
